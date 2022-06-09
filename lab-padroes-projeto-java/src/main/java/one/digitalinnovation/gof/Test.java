c5fdd0d52d39562f46fe77d2f96e2fa969b87d36
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-09 18:26:33 -0300

    exemplo Strategy

--- lab-padroes-projeto-java/src/main/java/one/digitalinnovation/gof/Test.java
+++ lab-padroes-projeto-java/src/main/java/one/digitalinnovation/gof/Test.java
@@ -1,7 +1,14 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
 public class Test {
     public static void main(String[] args){
+
+        //SINGLETON
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstancia();
@@ -17,7 +24,23 @@ public class Test {
         lazyHolder = SingletonLazyHolder.getInstancia();
         System.out.println(lazyHolder);
 
+        //STRATEGY
 
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
 
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
     }
 }
